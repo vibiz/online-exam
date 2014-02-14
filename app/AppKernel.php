5fde0543b571aeb23d0819bb46f93a273754f5d3
wilbros <willy.brordus.law@gmail.com>
2014-02-14 09:27:22 +0700

    add exam web bundle + config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Exam\DomainBundle\ExamDomainBundle(),
+            new Exam\WebBundle\ExamWebBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Exam/WebBundle/ExamWebBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: quantum
+ * Date: 2/14/14
+ * Time: 9:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Exam\WebBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ExamWebBundle extends Bundle{
+}
