2875ce1a8a07d8280c75b2edfc4b805ecc9dfabd
wilbros <willy.brordus.law@gmail.com>
2014-02-18 18:08:52 +0700

    add crud service

---
+++ src/Exam/WebBundle/Service/CRUDService.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: quantum
+ * Date: 2/18/14
+ * Time: 5:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Exam\WebBundle\Service;
+
+
+use Doctrine\ORM\EntityManager;
+use Exam\DomainBundle\Entity\Entity;
+use JMS\DiExtraBundle\Annotation\InjectParams;
+use JMS\DiExtraBundle\Annotation\Inject;
+use JMS\DiExtraBundle\Annotation\Service;
+
+/**
+ * Class CRUDService
+ * @package Exam\WebBundle\Service
+ * @Service("CRUDService")
+ */
+class CRUDService {
+    private $em;
+
+    /**
+     * @InjectParams({
+     *      "em" = @Inject("doctrine.orm.entity_manager")
+     * })
+     */
+    public function __construct(EntityManager $em) {
+        $this->em = $em;
+    }
+
+    public function update(Entity $entity) {
+        $this->em->persist($entity);
+    }
+
+}
