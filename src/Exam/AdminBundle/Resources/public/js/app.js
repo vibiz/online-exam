50663ac8c3431316d01d92c259103d8f28742e51
sam-adam <samuel.suhendra@gmail.com>
2014-02-18 18:53:32 +0700

    Update admin

--- src/Exam/AdminBundle/Resources/public/js/app.js
+++ src/Exam/AdminBundle/Resources/public/js/app.js
@@ -1,19 +1,45 @@
-$(function() {
+function Masker(mask) {
+    this.html = $("html");
+    this.masker = $("." + mask);
+
+
+    Masker.prototype.start = function() {
+        var doc = document.documentElement;
+        var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+        var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+
+        this.html.css("overflow", "hidden");
+        this.masker.css("top", top);
+        this.masker.show();
+    };
+
+    Masker.prototype.stop = function() {
+        this.html.css("overflow", "initial");
+        this.masker.css("top", 0);
+        this.masker.hide();
+    };
+}
+
+var $masker;
+
+$(document).ready(function() {
     // Side Bar Toggle
     $('.hide-sidebar').click(function() {
-	  $('#sidebar').hide('fast', function() {
-	  	$('#content').removeClass('span9');
-	  	$('#content').addClass('span12');
-	  	$('.hide-sidebar').hide();
-	  	$('.show-sidebar').show();
-	  });
-	});
+        $('#sidebar').hide('fast', function() {
+            $('#content').removeClass('span9');
+            $('#content').addClass('span12');
+            $('.hide-sidebar').hide();
+            $('.show-sidebar').show();
+        });
+    });
 
-	$('.show-sidebar').click(function() {
-		$('#content').removeClass('span12');
-	   	$('#content').addClass('span9');
-	   	$('.show-sidebar').hide();
-	   	$('.hide-sidebar').show();
-	  	$('#sidebar').show('fast');
-	});
+    $('.show-sidebar').click(function() {
+        $('#content').removeClass('span12');
+        $('#content').addClass('span9');
+        $('.show-sidebar').hide();
+        $('.hide-sidebar').show();
+        $('#sidebar').show('fast');
+    });
+
+    $masker = new Masker("mask");
 });
---
+++ src/Exam/DomainBundle/Repository/QuestionRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Exam\DomainBundle\Repository;
+
+use Doctrine\ORM\EntityManager;
+use Exam\DomainBundle\Repository\BaseRepository;
+use JMS\DiExtraBundle\Annotation\Inject;
+use JMS\DiExtraBundle\Annotation\InjectParams;
+use JMS\DiExtraBundle\Annotation\Service;
+
+/**
+ * @Service("questionRepo")
+ */
+class QuestionRepository extends BaseRepository {
+    private $service;
+    /**
+     * @InjectParams({
+     *      "em" = @Inject("doctrine.orm.entity_manager")
+     * })
+     */
+    public function __construct(EntityManager $em) {
+        parent::__construct($em);
+    }
+}
